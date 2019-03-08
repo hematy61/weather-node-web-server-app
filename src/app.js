7d7babb6ce6e8870d07982a1dc349fd5836ab6e7
Mohammad Hematy <hematy61@gmail.com>
2019-03-08 10:31:36 -0600

    Set up a basic route to home on port 3000

--- src/app.js
+++ src/app.js
@@ -1,1 +1,11 @@
-console.log('node js web server based app with express')
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('<h1>Express App</h1>')
+});
+
+app.listen(3000, () => {
+  console.log('server is running on port 3000')
+})
