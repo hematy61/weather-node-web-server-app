6d9148159ce501baf660f26e00031be739c33681
Mohammad Hematy <hematy61@gmail.com>
2019-03-15 19:06:25 -0500

    set up the request to Mapbox API to get longitude, latitude, and location

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -2,6 +2,49 @@ const request = require('request');
 
 const keys = require('../keys.json');
 
-console.log(keys.darksky)
+
+// making HTTP request to Mapbox API to get the latitude and longitude of the requested location.
+const geocode = (address, callback) => {
+  // preventing app crash when user not sending any location
+  if (address.length === 0){
+    address = 'Shiraz'
+  }
+  // define Mapbox api url to request data with 'request' package
+  const geocodeUrl = `https://api.mapbox.com/geocoding/v5/mapbox.places/${address}.json?access_token=${keys.mapbox}&limit=1`
+
+  // makig HTTP request to the defined url and set it to respond with JSON
+  request({
+    url: geocodeUrl,
+    json: true
+  }, (error, { body = {} }) => {
+    // request callback function which receive 3 arguments: error, res, and body. By destructuring "res" and removing
+    // body as it's optional, it is simplified from (error, res, body) to (error, { body = {}})
+
+    if(error){
+      // checking for any errors coming from the request. it can be anything that cause the request
+      // to be failed like "user is not connecting to internet" or "Mabbox servers are not responding"
+      // As there is an error, we sent the data undefined
+      callback('Unable to connect to the location service', undefined)
+    } else if (body.features.length === 0) {
+      // if request goes through and location didn't found, we are going to show this error to user. As 
+      // there is an error, we sent the data undefined
+      callback('Address Not Found', undefined)
+    } else {
+      // if there is no error, we send error as undefined and data an object to callback function
+      return callback(undefined, {
+        longitude: body.features[0].center[0],
+        latitude: body.features[0].center[1],
+        location: body.features[0].place_name
+      })
+    }
+  })
 
 
+}
+
+geocode('winnipeg', (error, data) => {
+  console.log("error", error)
+  console.log("data", data)
+})
+
+module.exports = geocode
