93d8ac7e53c20f17ad030e103d34c4d781c1271c
Mohammad Hematy <hematy61@gmail.com>
2019-03-17 10:34:30 -0500

    show received data from our API to user

--- public/js/index.js
+++ public/js/index.js
@@ -4,17 +4,28 @@ console.log('this is from index.js')
 
 const weatherForm = document.querySelector('form');
 const search = document.querySelector('input');
+const message_1 = document.querySelector('#message-1')
+const message_2 = document.querySelector('#message-2')
 
 weatherForm.addEventListener('submit', (e) => {
   e.preventDefault();
   const location = search.value;
+  message_1.textContent = ''
+  message_2.textContent = 'Loading ......'
   fetch(`http://localhost:3000/weather?address=${location}`)
     .then((res) => res.json())
     .then(data => {
+      // check for any error that comes from our API and if there is nothing then present received data 
+      // on our front end to user
       if (data.error) {
-        console.log(data.error)
+        message_1.textContent = `${data.error}`
+        message_2.textContent = ''
       }else {
-        console.log(data)
+        message_1.textContent = data.weatherData.location
+        message_2.textContent = `Right now it's ${data.weatherData.temperature}°C and 
+                                ${data.weatherData.summary} with ${data.weatherData.chanceOfRain}% 
+                                chance of Rain`
+
       }
     });
 })
