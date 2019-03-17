73bf19023f902466f65a454b0aef641f93d80bab
Mohammad Hematy <hematy61@gmail.com>
2019-03-16 22:58:36 -0500

    fix an app crash when user sends problematic inputs as address

--- src/app.js
+++ src/app.js
@@ -69,7 +69,7 @@ app.get('/weather', (req, res) => {
   }
   console.log("line 70: ",req.query.address)
 
-  geocode(req.query.address, (error, {latitude, longitude, location}) => {
+  geocode(req.query.address, (error, {latitude, longitude, location} = {}) => {
     if (error) {
       console.log("error line 74: ", error)
       return res.send({ error })
