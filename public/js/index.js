7b136d431d1264c469e38dcd078595599dbfb8a4
Mohammad Hematy <hematy61@gmail.com>
2019-05-26 12:58:20 -0500

    add scripts to update screen and show each day's detail

--- public/js/index.js
+++ public/js/index.js
@@ -3,7 +3,7 @@ const search = document.querySelector('input');
 const summary_display = document.querySelector('#summary-display');
 
 const top_left_display_icon = document.querySelector('.top-left-display-icon')
-const top_left_display_weather = document.querySelector('.top-left-display-weather')
+const top_left_display_date = document.querySelector('.top-left-display-date')
 const top_left_display_city = document.querySelector('.top-left-display-city')
 const top_left_display_temp = document.querySelector('.top-left-display-temp')
 
@@ -15,9 +15,11 @@ const sub_condition_value4 = document.querySelector('#sub-condition-value4')
 const days_display_left_arrow = document.querySelector('.days-display-left-arrow')
 const days_display_right_arrow = document.querySelector('.days-display-right-arrow')
 const days_display_next_day = document.querySelector('.days-display-next-day')
+const days_display_previous_day = document.querySelector('.days-display-previous-day')
 
-const data = {}
-const weekdays = ['Saturday', 'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
+let receivedData = {}
+let todayInDigit = 0;
+const weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
 
 weatherForm.addEventListener('submit', (e) => {
   e.preventDefault();
@@ -30,43 +32,59 @@ weatherForm.addEventListener('submit', (e) => {
         // on our front end to user
         summary_display.textContent = `${data.error}`
       } else {
-        const {
-          location,
-          body
-        } = data.weatherData
-        data = {location, body}
-        console.log(data)
-        updateScreen(data)
+        receivedData = data.weatherData
+        console.log(receivedData)
+        updateScreen(receivedData.location, receivedData.body.daily.data[0])
         days_display_right_arrow.style.display = `unset`
         days_display_next_day.style.display = `unset`
-        const tomorrow = new Date(body.daily.data[0].time * 1000).getDay()
-        days_display_next_day.textContent = `${weekdays[tomorrow]}`
+        days_display_next_day.textContent = `${weekdays[todayInDigit + 1]}`
       }
     })
 })
 
 
  days_display_left_arrow.addEventListener('click', (e) => {
-   const newData = 
-   updateScreen()
+   if ( (todayInDigit - 1) > 0) {
+     todayInDigit--
+     updateScreen(receivedData.location, receivedData.body.daily.data[todayInDigit])
+     days_display_next_day.textContent = `${weekdays[todayInDigit + 1]}`
+     days_display_previous_day.textContent = `${weekdays[todayInDigit - 1]}`
+   } else if ( (todayInDigit -1) === 0 ) {
+     todayInDigit--
+     days_display_previous_day.textContent = ''
+     days_display_left_arrow.style.display = ``
+
+   }
  })
+
  days_display_right_arrow.addEventListener('click', (e) => {
-   updateScreen()
+  if ( (todayInDigit + 1) < (receivedData.body.daily.data.length - 2) ) {
+    todayInDigit++
+    console.log('todayInDigit: ',todayInDigit)
+    updateScreen(receivedData.location, receivedData.body.daily.data[todayInDigit])
+    days_display_next_day.textContent = `${weekdays[todayInDigit + 1]}`
+    days_display_left_arrow.style.display = `unset`
+    days_display_previous_day.textContent = `${weekdays[todayInDigit - 1]}`
+  }
  })
 
- const updateScreen = (data) => {
-  top_left_display_icon.style.background = `url(../assets/${data.body.currently.icon}.svg) no-repeat top left`
+ const updateScreen = (location, data) => {
+   console.log(location, data)
+  top_left_display_icon.style.background = `url(../assets/${data.icon}.svg) no-repeat top left`
   top_left_display_icon.style.backgroundSize = `contain`
-  top_left_display_weather.textContent = data.body.currently.summary
-  top_left_display_city.textContent = data.location
-  top_left_display_temp.textContent = `${data.body.currently.temperature}°C`
+  console.log('todayInDigit from updateScreen: ', todayInDigit)
+  top_left_display_date.textContent = `${weekdays[todayInDigit]}`
+  top_left_display_city.textContent = location
+  top_left_display_temp.innerHTML = `
+  <p>Min: ${data.temperatureLow}°C</p>
+  <p>Max: ${data.temperatureHigh}°C</p>
+  `
+  summary_display.textContent = `${data.summary}`
 
-  summary_display.textContent = `${data.body.daily.summary}`
-
-  sub_condition_value1.textContent = `${data.body.currently.humidity * 100} %`
-  sub_condition_value2.textContent = `${data.body.currently.precipProbability * 100} %`
-  sub_condition_value3.textContent = `${(data.body.currently.windSpeed * 1.609).toFixed(2)} km/h`
-  sub_condition_value4.textContent = `${data.body.currently.pressure} PS`
+  sub_condition_value1.textContent = `${Math.round(data.humidity * 100)} %`
+  sub_condition_value2.textContent = `${data.precipProbability * 100} %`
+  sub_condition_value3.textContent = `${(data.windSpeed * 1.609).toFixed(2)} km/h`
+  sub_condition_value4.textContent = `${data.pressure} PS`
  }
 
  const get_date = (today) => {}
